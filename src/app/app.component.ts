fd93b181a384d5edd94349e7563068c79fcc2e09
joemust0 <joaofelipe.1607@gmail.com>
2024-02-14 22:12:13 -0300

    atualização - probelma com rotas no novo angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutComponent,RouterModule,RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'moments';
+  title = 'momentos';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./components/pages/home/home.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:  '', component: HomeComponent},
+    {path:  'about', component: AboutComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
