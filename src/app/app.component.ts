af7c44b18b25c36a87b020fa2c0aaf27f49c62c0
joemust0 <joaofelipe.1607@gmail.com>
2024-02-17 00:25:58 -0300

    Correção problemas rotas Angular 17.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutComponent,RouterModule,RouterLinkActive, RouterLink],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutComponent,RouterModule,RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from "./components/pages/home/home.component";
 import { AboutComponent } from "./components/pages/about/about.component";
 
 export const routes: Routes = [
-    {path:  '', component: HomeComponent},
-    {path:  'about', component: AboutComponent}
+    {'path':  '', component: HomeComponent},
+    {'path':  'about', component: AboutComponent}
+  
 ];
 
 @NgModule({
