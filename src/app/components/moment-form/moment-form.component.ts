196e89c5d7dde3f77dfa203d6ad44275d63abb7a
joemust0 <joaofelipe.1607@gmail.com>
2024-02-17 19:25:38 -0300

    validação de formulario reativo 28.

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,19 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 export class MomentFormComponent implements OnInit{
 @Input() btnText!: string;
 
+momentForm!: FormGroup;
+
 constructor() {}
 
 ngOnInit(): void {
-  
+  this.momentForm = new FormGroup({
+    id: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    image: new FormControl(''),
+  });
 }
+
+get title() {
+  return this.momentForm.get('title')!;
+}
+
+get description() {
+  return this.momentForm.get('description')!;
+}
+submit(){
+  if(this.momentForm.invalid) {
+    return;
+  }
+  console.log("Enviou o formulário...")
+  }
 }
