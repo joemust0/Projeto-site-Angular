d220702ce89a7adb015e6a71fa0064a00bb2e87e
joemust0 <joaofelipe.1607@gmail.com>
2024-02-17 02:02:30 -0300

    Layuot e css da página 25.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { CommonModule } from '@angular/common';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutComponent,RouterModule,RouterLinkActive, RouterLink, CommonModule],
+  imports: [RouterOutlet, NewMomentComponent,HeaderComponent, FooterComponent, HomeComponent, AboutComponent,RouterModule,RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-new-moment',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent {
+
+}
